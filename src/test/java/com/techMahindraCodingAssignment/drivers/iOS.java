c29b3c0fde0ec1ad7ccf4e1ff23b43bfff4fbe47
ravisingh171989 <Anand@Anand-MacBook-Pro.local>
2020-05-07 00:55:51 +0530

    added flexibility of additional platform of iOS

---
+++ src/test/java/com/techMahindraCodingAssignment/drivers/iOS.java
@@ -0,0 +1,15 @@
+package com.techMahindraCodingAssignment.drivers;
+
+/*iOS device related details*/
+public class iOS {
+	public static final String platformName = "iOS";
+	public static final String platformVersion = "11.3.1";
+	public static final String deviceName = "NotUsed";
+	public static final String device = "iOS";
+	public static final String udid = "1234####123";
+	public static final String URL = "http://127.0.0.1:4723/wd/hub";
+	public static final String browserNameSafari = "Safari";
+	public static final String appName = "**.ipa";
+	public static final String appDir = "";
+	public static final String automationName = "XCUITest";
+}
